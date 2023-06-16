258c1b3060a7eec805508296839e7a17304ef3f1
abinet55 <abwonde@gmail.com>
2023-06-16 10:25:33 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(void)
-/* this is the start of the main function */
+/**
+ * this is the start of the main function
+ */
 
 {
-puts(""Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 
 return (0);
 }
