a29929aa288c363303267c5095227031ef0779eb
abinet55 <abwonde@gmail.com>
2023-06-19 11:09:13 -0700

    print statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,13 @@
 /**
 * main - function print out the message
 *
-* Return: always zero (0) 
+* Return: always zero (0)
 */
 
 int main(void)
 {
 
 puts("\"programming is like building a multilingual puzzle");
-
-puts("");
 
 return (0);
 
