247d91132e67d847eec1f2b18d611ce53f2aad2a
abinet55 <abwonde@gmail.com>
2023-06-19 09:32:00 -0700

    printing base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - prints base 16
+*
+* Return: always 0
+*/
+
+int main(void)
+
+{
+  char alphabet;
+int num;
+
+for (num = 0; num <= 9; num++)
+
+putchar(num + '0' );
+
+for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+
+putchar(alphabet);
+  
+putchar('\n');
+
+return (0);
+
+}
