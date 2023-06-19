27a7fd96b2d1360b0a880d4a31e7b0f45a18296a
abinet55 <abwonde@gmail.com>
2023-06-19 11:20:13 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - print a sentence
+*
+* Return: always zero (0)
+*/
+
+int main(void)
+
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+
+}
