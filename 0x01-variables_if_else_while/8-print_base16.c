03eaa31b6d1f357acdd5b7d9eb58d25af788a703
abinet55 <abwonde@gmail.com>
2023-06-19 09:35:49 -0700

    printing base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,17 +9,19 @@
 int main(void)
 
 {
-  char alphabet;
+
+char alphabet;
+
 int num;
 
-for (num = 0; num <= 9; num++)
+for(num = 0; num <= 9; num++)
 
 putchar(num + '0' );
 
-for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+for(alphabet = 'a'; alphabet <= 'f'; alphabet++)
 
 putchar(alphabet);
-  
+
 putchar('\n');
 
 return (0);
