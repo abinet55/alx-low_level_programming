29ceb5957645739c676855bd31b535ffd6ab7d64
abinet55 <abwonde@gmail.com>
2023-06-21 11:11:36 -0700

    prints absolute value of the input

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _abs - prints absolute value
+* @n: input
+* Return: always zero
+*/
+
+int _abs(int n)
+
+{
+if (n < 0)
+{
+int abs_val = n * (-1);
+
+return (abs_val);
+
+}
+else
+
+return (n);
+
+}
