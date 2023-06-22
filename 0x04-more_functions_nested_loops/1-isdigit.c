d58e36161564bf0704df18d811664e79baf327bc
abinet55 <abwonde@gmail.com>
2023-06-22 14:32:50 -0700

    checks if it is digit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isdigit - prints and checks digit
+* @c: input
+* Return: always 0
+*/
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+
+return (1);
+
+else
+
+return (0);
+
+}
