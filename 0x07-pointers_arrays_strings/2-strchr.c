fb1bc451f186e2af57d246a868f8d96f53c04fd5
abinet55 <abwonde@gmail.com>
2023-07-04 10:36:58 -0700

    using strchr fuction

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* _strchr - search for character
+* @s: string to be search
+* @c: character to be search
+* Return: character
+*/
+
+char *_strchr(char *s, char c)
+{
+while(*s)
+{
+if (*s != c)
+{
+s++;
+}
+else
+return (s);
+}
+if (c == '\0')
+return (s);
+return (NULL);
+}
