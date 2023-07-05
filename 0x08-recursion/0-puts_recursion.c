db90772b31af737e858ec906d9c5f5fd1f328238
abinet55 <abwonde@gmail.com>
2023-07-05 09:35:32 -0700

    prints string using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts_recursion - prints string
+* @s: pointer to string
+* Return: nothing
+*/
+
+void _puts_recursion(char *s)
+{
+  int i;
+  for (i = 0; s[i] != '\0'; i++)
+    {
+      _putchar(s[i]);
+    }
+  _putchar('\n');
+}
