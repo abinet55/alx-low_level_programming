940d971a21f2112144e835e65a4a327a4da51098
abinet55 <abwonde@gmail.com>
2023-07-17 11:18:32 -0700

    project bout poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H_
 #define DOG_H_
+/**
+* struct dog - struct inputs
+* @name: name of dog
+* @age: age of dog
+* @owner: owner
+* Description: about the dog
+*/
 
 typedef struct dog
 {
