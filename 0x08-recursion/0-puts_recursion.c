db0cb9dac5fff26e78588b7eae2e3ad7f303c9b5
abinet55 <abwonde@gmail.com>
2024-01-10 10:24:51 -0800

    prints string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _puts_recursion - writes string
+* @s: the string to print
+* Return: nothing
+*/
+
+void _puts_recursion(char *s)
+{
+if (s[0] != '\0')
+{
+_putchar(s[0]);
+_puts_recursion(s+1);
+}
+ else
+_putchar('\n');
+}
