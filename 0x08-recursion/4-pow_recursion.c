bc8f000297634b389f0ee3c551ca32a963291b1f
abinet55 <abwonde@gmail.com>
2024-01-10 12:13:51 -0800

    calculate power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,8 +10,8 @@ int _pow_recursion(int x, int y)
 {
 if (y < 0)
 return (-1);
-if (y == 1)
+if (y == 0)
 return (x);
 else
-return (x * _pow_recursion(x, y-1));
+return (x * _pow_recursion(x, y - 1));
 }
