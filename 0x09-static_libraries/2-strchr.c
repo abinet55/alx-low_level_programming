bb784705b2901de51f98a7149c7b03b170626562
abinet55 <abwonde@gmail.com>
2024-01-12 11:52:48 -0800

    mnfn

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strchr - locates a char in a string
+* @s: the string to check
+* @c: char to find
+* Return: pointer to the first char ocurrency.
+*/
+char *_strchr(char *s, char c)
+{
+}
