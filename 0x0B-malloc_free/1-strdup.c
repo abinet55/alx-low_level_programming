64459a704707c412d1534abe496e3c30d006fe25
abinet55 <abwonde@gmail.com>
2024-01-15 09:15:47 -0800

    returns pointer

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,7 @@ char *strout;
 unsigned int x, y;
 if (str == NULL)
 return (NULL);
-for (x = 0; str[x] != '\0'; x++);
+for (x = 0; str[x] != '\0'; x++)
 strout = (char *)malloc(sizeof(char) * (x + 1));
 if (strout == NULL)
 return (NULL);
