f86979c505ac2f1b0756b1364fa3f6ee55c2abce
abinet55 <abwonde@gmail.com>
2024-02-01 10:37:26 -0800

    prints all element

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,13 +11,13 @@ size_t print_list(const list_t *h)
 size_t element = 0;
 while (h != NULL)
 {
-if (h -> str != NULL)
+if (h->str != NULL)
 {
 printf("[%d] %s\n", h->len, h->str);
 }
 else
 printf("[%d] %s\n", 0, "(nil)");
-h = h -> next;
+h = h->next;
 element++;
 }
 return(element);
